921e384c014b7ad6a2fdea4a52d66c7ce4f5ba76
Igor Muller <igormullersantos@hotmail.com>
2023-05-10 10:40:07 -0300

    Criação da estrutura da HomeView e dos components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,43 @@
 <script setup>
+import LogosSection from '../components/LogosSection.vue';
+import About from '../components/About.vue';
+import Works from '../components/Works.vue';
+import Clients from '../components/Clients.vue';
 
 </script>
 
 <template>
+    <section id="Hero">
+        
+        <div class="heroContent">
+
+        </div>
+
+        <div class="heroImg">
+
+        </div>
+
+    </section>
+
+    <section id="logos">
+        <LogosSection />
+    </section>
+
+    <section id="about">
+        <About />
+    </section>
+
+    <section id="works">
+        <Works />
+    </section>
+
+    <section id="clients">
+        <Clients />
+    </section>
+
+    <section id="contact">
+
+    </section>
 
 </template>
 
